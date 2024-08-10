e325501bd13ac26041e122b0350c525ac04da3d1
trwfs00 <119293977+trwfs00@users.noreply.github.com>
2024-08-10 21:47:39 +0700

    feat: add metadata and favicon

--- main.py
+++ main.py
@@ -24,236 +24,180 @@ app, Rt = fast_app()
 
 @Rt('/')
 def get():
-    return Div(
-        Link(href="/static/style.css", rel="stylesheet"),  # Include the CSS
-        Style('''
-            * {
-                color: black; /* Set all text to black */
-            }
+    return Html(
+        Head(
+            Title('DCA Calculator'),
+            Meta(name="description", content="DCA Calculator for calculating Dollar-Cost Averaging investments."),
+            Meta(name="keywords", content="DCA, Dollar-Cost Averaging, Investment Calculator, Stocks"),
+            Meta(name="author", content="Your Name"),
+            Link(rel="apple-touch-icon", sizes="180x180", href="/static/apple-touch-icon.png"),
+            Link(rel="icon", type="image/png", sizes="32x32", href="/static/favicon-32x32.png"),
+            Link(rel="icon", type="image/png", sizes="16x16", href="/static/favicon-16x16.png"),
+            Link(rel="manifest", href="/static/site.webmanifest"),
+            Link(rel="icon", href="/static/favicon.ico", type="image/x-icon"),
+            Link(href="/static/style.css", rel="stylesheet"),
+            Link(href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css", rel="stylesheet"),
+            Script(src="https://cdn.jsdelivr.net/gh/gnat/css-scope-inline@main/script.js"),
+            Script(src="https://cdn.jsdelivr.net/gh/answerdotai/surreal@1.3.0/surreal.js"),
+            Script(src="https://unpkg.com/htmx.org@next/dist/htmx.min.js"),
+            Script(f'''
+                function openModal(content) {{
+                    document.getElementById('modal-content').innerHTML = content;
+                    document.getElementById('modal').style.display = 'block';
+                }}
 
-            .autocomplete {
-                position: relative;
-                display: inline-block;
-            }
+                function closeModal() {{
+                    document.getElementById('modal').style.display = 'none';
+                }}
 
-            .autocomplete-items {
-                position: absolute;
-                border: 1px solid #d4d4d4;
-                border-bottom: none;
-                border-top: none;
-                z-index: 99;
-                top: 100%;
-                left: 0;
-                right: 0;
-                background-color: #fff;
-            }
+                var nasdaq100 = {NASDAQ_100_SYMBOLS};
 
-            .autocomplete-items div {
-                padding: 10px;
-                cursor: pointer;
-                background-color: #fff; 
-                border-bottom: 1px solid #d4d4d4; 
-            }
-
-            .autocomplete-items div:hover {
-                background-color: #e9e9e9; 
-            }
-
-            .autocomplete-active {
-                background-color: DodgerBlue !important; 
-                color: #ffffff; 
-            }
-
-            .modal {
-                display: none;
-                position: fixed;
-                z-index: 1;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                overflow: auto;
-                background-color: rgb(0,0,0);
-                background-color: rgba(0,0,0,0.4);
-                padding-top: 60px;
-            }
-
-            .modal-content {
-                background-color: #13171f;
-                margin: 5% auto;
-                padding: 20px;
-                border: 1px solid #888;
-                width: 50%;
-                border-radius: 10px;
-            }
-
-            .close {
-                color: #aaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-            }
-
-            .close:hover,
-            .close:focus {
-                color: black;
-                text-decoration: none;
-                cursor: pointer;
-            }
-        '''),
-        Script(f'''
-            function openModal(content) {{
-                document.getElementById('modal-content').innerHTML = content;
-                document.getElementById('modal').style.display = 'block';
-            }}
-
-            function closeModal() {{
-                document.getElementById('modal').style.display = 'none';
-            }}
-
-            var nasdaq100 = {NASDAQ_100_SYMBOLS};
-
-            function autocomplete(inp) {{
-                var currentFocus;
-                inp.addEventListener("input", function(e) {{
-                    var a, b, i, val = this.value;
-                    closeAllLists();
-                    if (!val) {{ return false; }}
-                    currentFocus = -1;
-                    a = document.createElement("DIV");
-                    a.setAttribute("id", this.id + "autocomplete-list");
-                    a.setAttribute("class", "autocomplete-items");
-                    this.parentNode.appendChild(a);
-                    for (i = 0; i < nasdaq100.length; i++) {{
-                        if (nasdaq100[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {{
-                            b = document.createElement("DIV");
-                            b.innerHTML = "<strong>" + nasdaq100[i].substr(0, val.length) + "</strong>";
-                            b.innerHTML += nasdaq100[i].substr(val.length);
-                            b.innerHTML += "<input type='hidden' value='" + nasdaq100[i] + "'>";
-                            b.addEventListener("click", function(e) {{
-                                inp.value = this.getElementsByTagName("input")[0].value;
-                                closeAllLists();
-                            }});
-                            a.appendChild(b);
+                function autocomplete(inp) {{
+                    var currentFocus;
+                    inp.addEventListener("input", function(e) {{
+                        var a, b, i, val = this.value;
+                        closeAllLists();
+                        if (!val) {{ return false; }}
+                        currentFocus = -1;
+                        a = document.createElement("DIV");
+                        a.setAttribute("id", this.id + "autocomplete-list");
+                        a.setAttribute("class", "autocomplete-items");
+                        this.parentNode.appendChild(a);
+                        for (i = 0; i < nasdaq100.length; i++) {{
+                            if (nasdaq100[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {{
+                                b = document.createElement("DIV");
+                                b.innerHTML = "<strong>" + nasdaq100[i].substr(0, val.length) + "</strong>";
+                                b.innerHTML += nasdaq100[i].substr(val.length);
+                                b.innerHTML += "<input type='hidden' value='" + nasdaq100[i] + "'>";
+                                b.addEventListener("click", function(e) {{
+                                    inp.value = this.getElementsByTagName("input")[0].value;
+                                    closeAllLists();
+                                }});
+                                a.appendChild(b);
+                            }}
+                        }}
+                    }});
+                    inp.addEventListener("keydown", function(e) {{
+                        var x = document.getElementById(this.id + "autocomplete-list");
+                        if (x) x = x.getElementsByTagName("div");
+                        if (e.keyCode == 40) {{
+                            currentFocus++;
+                            addActive(x);
+                        }} else if (e.keyCode == 38) {{
+                            currentFocus--;
+                            addActive(x);
+                        }} else if (e.keyCode == 13) {{
+                            e.preventDefault();
+                            if (currentFocus > -1) {{
+                                if (x) x[currentFocus].click();
+                            }}
+                        }}
+                    }});
+                    function addActive(x) {{
+                        if (!x) return false;
+                        removeActive(x);
+                        if (currentFocus >= x.length) currentFocus = 0;
+                        if (currentFocus < 0) currentFocus = (x.length - 1);
+                        x[currentFocus].classList.add("autocomplete-active");
+                    }}
+                    function removeActive(x) {{
+                        for (var i = 0; i < x.length; i++) {{
+                            x[i].classList.remove("autocomplete-active");
                         }}
                     }}
-                }});
-                inp.addEventListener("keydown", function(e) {{
-                    var x = document.getElementById(this.id + "autocomplete-list");
-                    if (x) x = x.getElementsByTagName("div");
-                    if (e.keyCode == 40) {{
-                        currentFocus++;
-                        addActive(x);
-                    }} else if (e.keyCode == 38) {{
-                        currentFocus--;
-                        addActive(x);
-                    }} else if (e.keyCode == 13) {{
-                        e.preventDefault();
-                        if (currentFocus > -1) {{
-                            if (x) x[currentFocus].click();
+                    function closeAllLists(elmnt) {{
+                        var x = document.getElementsByClassName("autocomplete-items");
+                        for (var i = 0; i < x.length; i++) {{
+                            if (elmnt != x[i] && elmnt != inp) {{
+                                x[i].parentNode.removeChild(x[i]);
+                            }}
                         }}
                     }}
-                }});
-                function addActive(x) {{
-                    if (!x) return false;
-                    removeActive(x);
-                    if (currentFocus >= x.length) currentFocus = 0;
-                    if (currentFocus < 0) currentFocus = (x.length - 1);
-                    x[currentFocus].classList.add("autocomplete-active");
+                    document.addEventListener("click", function (e) {{
+                        closeAllLists(e.target);
+                    }});
                 }}
-                function removeActive(x) {{
-                    for (var i = 0; i < x.length; i++) {{
-                        x[i].classList.remove("autocomplete-active");
+
+                function toggleInputs() {{
+                    var stockDiv = document.getElementById('stock-symbol-div');
+                    var customRateDiv = document.getElementById('custom-rate-div');
+                    var rateSource = document.querySelector('input[name="rate_source"]:checked').value;
+                    if (rateSource === 'stock') {{
+                        stockDiv.style.display = 'block';
+                        customRateDiv.style.display = 'none';
+                    }} else {{
+                        stockDiv.style.display = 'none';
+                        customRateDiv.style.display = 'block';
                     }}
                 }}
-                function closeAllLists(elmnt) {{
-                    var x = document.getElementsByClassName("autocomplete-items");
-                    for (var i = 0; i < x.length; i++) {{
-                        if (elmnt != x[i] && elmnt != inp) {{
-                            x[i].parentNode.removeChild(x[i]);
-                        }}
-                    }}
-                }}
-                document.addEventListener("click", function (e) {{
-                    closeAllLists(e.target);
+
+                document.addEventListener('DOMContentLoaded', function() {{
+                    autocomplete(document.getElementById('stock'));
+                    toggleInputs();  // Set initial visibility based on default selection
                 }});
-            }}
-
-            function toggleInputs() {{
-                var stockDiv = document.getElementById('stock-symbol-div');
-                var customRateDiv = document.getElementById('custom-rate-div');
-                var rateSource = document.querySelector('input[name="rate_source"]:checked').value;
-                if (rateSource === 'stock') {{
-                    stockDiv.style.display = 'block';
-                    customRateDiv.style.display = 'none';
-                }} else {{
-                    stockDiv.style.display = 'none';
-                    customRateDiv.style.display = 'block';
-                }}
-            }}
-
-            document.addEventListener('DOMContentLoaded', function() {{
-                autocomplete(document.getElementById('stock'));
-                toggleInputs();  // Set initial visibility based on default selection
-            }});
-        '''),
-        Div(
-            H1('DCA Investment Calculator'),
-            Form(
-                P('Currency:'),
-                Select(
-                    Option('THB', value='THB'),
-                    Option('USD', value='USD'),
-                    Option('EUR', value='EUR'),
-                    Option('JPY', value='JPY'),
-                    Option('GBP', value='GBP'),
-                    name='currency'
-                ),
-                P('Choose interest rate source:'),
-                Label(
-                    Input(name='rate_source', type='radio', value='stock', checked=True, onclick="toggleInputs()"),
-                    'Use stock historical growth rate'
-                ),
-                Label(
-                    Input(name='rate_source', type='radio', value='custom', onclick="toggleInputs()"),
-                    'Use custom annual interest rate'
-                ),
-                Div(
-                    P('Stock Symbol:'),
-                    Div(
-                        Input(name='stock', type='text', id='stock'),  # Added id for autocomplete
-                        _class='autocomplete'  # Wrap input in a div for relative positioning
-                    ),
-                    id='stock-symbol-div'  # This div will be toggled
-                ),
-                Div(
-                    P('Custom Annual Interest Rate (%):'),
-                    Input(name='custom_interest_rate', type='number', step='0.01', value='10'),
-                    id='custom-rate-div'  # This div will be toggled
-                ),
-                P('Initial Investment (per month):'),
-                Input(name='initialInvestment', type='number', value='5000'),
-                P('Duration (years):'),
-                Input(name='duration', type='number', value='15'),
-                P('Annual Increase (%):'),
-                Input(name='annualIncrease', type='number', value='25'),
-                Button('Calculate', type='submit'),
-                action='/calculate', method='post', hx_post='/calculate', hx_target='#result'
-            ),
-            Div(id='result'),
-            _class='container'  # Add a class to the container
+            ''', _ctype='text/javascript', defer=True),
         ),
-        # Modal structure
-        Div(
+        Body(
             Div(
-                Span('X', _class='close', onclick='closeModal()'),
-                Div(id='modal-content'),
-                _class='modal-content'
-            ),
-            _class='modal',
-            id='modal'
-        ),
-        _class='wrapper'  # Add a class to the wrapper for centering
+                Div(
+                    H1('DCA Investment Calculator'),
+                    Form(
+                        P('Currency:'),
+                        Select(
+                            Option('THB', value='THB'),
+                            Option('USD', value='USD'),
+                            Option('EUR', value='EUR'),
+                            Option('JPY', value='JPY'),
+                            Option('GBP', value='GBP'),
+                            name='currency'
+                        ),
+                        P('Choose interest rate source:'),
+                        Label(
+                            Input(name='rate_source', type='radio', value='stock', checked=True, onclick="toggleInputs()"),
+                            'Use stock historical growth rate'
+                        ),
+                        Label(
+                            Input(name='rate_source', type='radio', value='custom', onclick="toggleInputs()"),
+                            'Use custom annual interest rate'
+                        ),
+                        Div(
+                            P('Stock Symbol:'),
+                            Div(
+                                Input(name='stock', type='text', id='stock'),  # Added id for autocomplete
+                                _class='autocomplete'  # Wrap input in a div for relative positioning
+                            ),
+                            id='stock-symbol-div'  # This div will be toggled
+                        ),
+                        Div(
+                            P('Custom Annual Interest Rate (%):'),
+                            Input(name='custom_interest_rate', type='number', step='0.01', value='10'),
+                            id='custom-rate-div'  # This div will be toggled
+                        ),
+                        P('Initial Investment (per month):'),
+                        Input(name='initialInvestment', type='number', value='5000'),
+                        P('Duration (years):'),
+                        Input(name='duration', type='number', value='15'),
+                        P('Annual Increase (%):'),
+                        Input(name='annualIncrease', type='number', value='25'),
+                        Button('Calculate', type='submit'),
+                        action='/calculate', method='post', hx_post='/calculate', hx_target='#result'
+                    ),
+                    Div(id='result'),
+                    _class='container'  # Add a class to the container
+                ),
+                # Modal structure
+                Div(
+                    Div(
+                        Span('X', _class='close', onclick='closeModal()'),
+                        Div(id='modal-content'),
+                        _class='modal-content'
+                    ),
+                    _class='modal',
+                    id='modal'
+                ),
+                _class='wrapper'  # Add a class to the wrapper for centering
+            )
+        )
     )
 
 # Route to serve static files
@@ -339,7 +283,8 @@ async def calculate(request):
 
     # Inject the result into the modal and open it
     modal_content = f'''
-    <h2>Total Investment Value: {formatted_value} {currency}</h2>
+    <h4>Total Investment Value:</h4>
+    <h1>{formatted_value} {currency}</h1>
     <p>Calculation complete.</p>
     '''
     return Script(f'openModal(`{modal_content}`);')
